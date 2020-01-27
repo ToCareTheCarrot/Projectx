96ee9bf6f5c3229a70dd2fd8b55d4e8ff7a8e77c
Your Name <you@example.com>
2020-01-27 19:45:59 +0300

    первое приближение

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Project} from './Components/Project';
+
+export class App extends React.Component{
+  state = {
+    projects: [
+      {id: 0, title: "Learn JS", done: true},
+      {id: 1, title: "Learn React", done: false},
+      {id: 2, title: "Learn React Router", done: false},
+      {id: 3, title: "Learn Redux", done: false},
+    ]
+  }
+
+  doneTask = (projects,id,fieldToUpdate) => {
+ 
+    const indexToChange = projects.map(projects => projects.id).indexOf(id);
+    const projectToUpdate = projects[indexToChange];
+    const projectCopy = {...projectToUpdate,...{done: fieldToUpdate}};
+    const updateProjects = [
+      ...projects.slice(0,indexToChange),
+      projectCopy,
+      ...projects.slice(indexToChange+1)
+    ];
+    this.setState({projects: updateProjects});
+  }
+
+  render(){
+    const {projects} = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="top">Projects:</h1>
+        {projects.map(project => (
+          <Project 
+            key={project.id} 
+            project={project}
+            doneTask={() => this.doneTask(this.state.projects, project.id, !project.done)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App;
+
